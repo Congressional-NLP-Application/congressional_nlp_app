148545d0b1695eee55ba14eb37a33d5ea4879624
Alex Lucchesi <84862301+lucchesia7@users.noreply.github.com>
2022-02-14 10:48:38 -0800

    feature update
    Created functions to create Sqlite3 db and table to house tweets from senate

--- alex_production/fcs.py
+++ alex_production/fcs.py
@@ -1,8 +1,11 @@
 from os import getenv
+from re import S
 from dotenv import load_dotenv
 import pandas as pd
+import sqlite3
 import tweepy
 
+db = "tweets.db"
 load_dotenv(r"C:\Users\Alex Lucchesi\OneDrive\Documents\GitHub\congressional_sentiment_NLP\alex_production\.env")
 ak = getenv("API_KEY")
 aks = getenv("API_KEY_SECRET")
@@ -18,7 +21,43 @@ handles = pd.read_csv('twitter_handles.csv')
 handles = handles.twitter_handle
 
 def get_tweets():
+    l = []
     for handle in handles:
         tweets = api.user_timeline(screen_name = str(handle))
         for tweet in tweets:
-            print(handle, tweet.text)
+            l.extend([[handle, tweet.text]])
+    return l
+            
+def create_table():
+    con = sqlite3.connect(db)
+    cur = con.cursor()
+    print("Connection Created to Database")
+    cur.execute(''' CREATE TABLE IF NOT EXISTS tweets
+                  (handle, tweet_text)''')
+    con.commit()
+    con.close()
+    print("Table successfully Created")
+    
+def add_tweets():
+    con = sqlite3.connect(db)
+    cur = con.cursor()
+    print("Successful Connection to Database")
+    
+    t = get_tweets()
+    cur.executemany("insert into tweets values (?, ?)", t)
+    con.commit()
+    con.close()
+    print("Tweets successfully inserted into table")
+    
+def drop_table():
+    con = sqlite3.connect(db)
+    cur = con.cursor()
+    print("Connection created to database")
+    
+    cur.execute(' DELETE FROM tweets')
+    con.commit()
+    con.close()
+    print("Table successfully deleted")
+    
+
+
